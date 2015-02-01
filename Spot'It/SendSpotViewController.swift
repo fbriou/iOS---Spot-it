64c74ab5437563d43443da585324f713e3830f9d
fbriou <MacBriou@pc24.home>
2015-02-01 23:20:05 +0100

    Add a button for the image Picker while I find why the recognizer gesture does not work

--- Spot'It/SendSpotViewController.swift
+++ Spot'It/SendSpotViewController.swift
@@ -23,20 +23,32 @@ class SendSpotViewController: UIViewController, UIImagePickerControllerDelegate,
         let recognizer = UITapGestureRecognizer(target: self, action: "displayImagePicker:")
         recognizer.numberOfTapsRequired = 1
         
-        //Add the recognizer to the image view
         imageView.addGestureRecognizer(recognizer)
-        
     }
     
     
-    func displayImagePicker(recog : UIGestureRecognizer){
-        
+    func displayImagePicker (recog:UITapGestureRecognizer){
+        println("taptap")
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
+        
         imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
         self.presentViewController(imagePicker, animated: true, completion: nil)
         
     }
+    
+    @IBAction func AddImage(sender: AnyObject) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+        
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -78,13 +90,16 @@ class SendSpotViewController: UIViewController, UIImagePickerControllerDelegate,
     }
     
     
-    //MARK: Image Picker Controller Delegate
+    
+    // MARK: Image Picker Controller Delegate
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-        
-        imageView.contentMode = UIViewContentMode.ScaleAspectFill
+        //imageView.contentMode = UIViewContentMode.ScaleAspectFill
         imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
         picker.dismissViewControllerAnimated(true, completion: nil)
+        
     }
+
     
     
     
